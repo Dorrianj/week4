52738b96c0862e48884eaf3171c3e31549c083a6
Dorrianj <52667212+Dorrianj@users.noreply.github.com>
2019-07-30 13:57:50 -0400

    Add files via upload

---
+++ outining.java
@@ -0,0 +1,39 @@
+package dateIdeas;
+
+import java.util.Scanner;
+
+public class outining {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sncr = new Scanner(System.in);
+		
+		int blue = 0;
+		String park = "play on swings";
+		String movies = "go to the movies";
+		String dinner = "go out for lunch.... its up to your budget";
+		String arcade = "go play video gmaes.... depending on price";
+		String picnic = "have lunch at the park";
+		String camping = "camp in the woods an have a bombfire";
+		String boat = "go on a boat ride or canuing";
+		
+		String name = "";
+		
+		
+		System.out.println("welcome to your date");
+		
+		System.out.println("would you like a indoor or outdoor date");
+		name= sncr.next();
+		
+		System.out.println("whats your budget");
+		blue= sncr.nextInt();
+		
+		
+		
+}
+
+	
+}
+	
+	
