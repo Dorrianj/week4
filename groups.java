38eabef416e824c72803ca1e7f49ca72f8c720f1
Dorrianj <52667212+Dorrianj@users.noreply.github.com>
2019-08-05 10:47:45 -0400

    Add files via upload

---
+++ groups.java
@@ -0,0 +1,21 @@
+package lastDay;
+
+import java.util.Scanner;
+
+public class groups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner java = new Scanner(System.in);
+		
+		for(int i = 1; i < 100; i += 2) {
+			System.out.println(i);
+			
+			//setup
+		}
+		
+	}
+
+}
+                                      
